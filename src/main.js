c37b1752e6a74e050e63df951c42a22847bb9b2e
Kostiantyn Voskov <kvoskov@gmail.com>
2023-03-25 17:39:26 +0200

    adding dist with updating vue config file

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/js/bootstrap.js"
 
 
 Vue.config.productionTip = false
@@ -10,4 +11,3 @@ new Vue({
   render: h => h(App),
 }).$mount('#app')
 
-import "bootstrap/dist/js/bootstrap.js"
